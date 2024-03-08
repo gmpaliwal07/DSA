ee9803192e6238ac29c8b799099ac181586ab21e
Gaurav <gmpaliwal21@gmail.com>
2024-03-08 16:00:26 +0530

    deleting-exe-file

---
+++ Linked List/linkedList.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif
+int n;
+cin >> n;
+vector<int> arr;
+for(auto it = 0; it <n; it++){
+    int a;
+    cin >> a;
+    cout << "  " << a << "  ";
+}
+return 0;
+}
