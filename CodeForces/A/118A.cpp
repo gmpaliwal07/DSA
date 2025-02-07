6d35a054d7e7041244b519a2c2ea2869be5d4b28
Jackal <jackal07@kali>
2025-02-07 15:45:53 +0530

    Fixes

--- CodeForces/A/118A.cpp
+++ CodeForces/A/118A.cpp
@@ -1,24 +1,46 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 #define ll long long int
-
+#define ld  long double
+#define mod 1000000007
+#define inf 1e18
+#define pb push_back
+#define vi vector<ll>
+#define vs vector<string>
+#define pii pair<ll,ll>
+#define ump unordered_map
+#define mp map
+#define pq_max priority_queue<ll>
+#define pq_min priority_queue<ll,vi,greater<ll>>
+#define ff first
+#define ss second
+#define mid(l,r) (l+(r-l)/2)
+#define loop(i,a,b) for(ll i=a;i<b;i++)
+#define rloop(i,a,b) for(ll i=a;i>=b;i--)
+#define logarr(arr,a,b) for(ll i=a;i<b;i++) cout<<arr[i]<<' '; cout<<endl;
 void file_i_o()
 {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+ios_base::sync_with_stdio(0);
+cin.tie(0);
+cout.tie(0);
 #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
 #endif
 }
 
 bool isVowel(char ch) {
     return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
-            ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U');
+         ch == 'y'|| ch == 'Y' ||ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U');
 }
-
-int main() {
+int main(int argc, char const *argv[])
+{
+clock_t begin = clock();
+file_i_o();
+#ifndef ONLINE_JUDGE
+clock_t end = clock();
+cout<<"Time: "<<(double)(end-begin)/CLOCKS_PER_SEC*1000<<"s\n";
+#endif
     file_i_o();
 
     string tour;
@@ -28,9 +50,12 @@ int main() {
     for (char ch : tour) {
         if (!isVowel(ch)) {
             newTour += '.';
-            newTour += ch;
+            newTour += tolower(ch); // Convert each character to lowercase properly
         }
     }
-    
-    return 0;
+
+    cout << newTour << endl; // Proper output formatting
+
+
+return 0;
 }
