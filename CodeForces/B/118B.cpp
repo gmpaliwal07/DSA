fbdc494e31abe7df26ccb83ba090d9f11997c386
Gaurav <gmpaliwal21@gmail.com>
2025-02-24 14:51:00 +0530

    Minor Chages

---
+++ CodeForces/B/118B.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define ld  long double
+#define mod 1000000007
+#define inf 1e18
+#define pb push_back
+#define vi vector<ll>
+#define vs vector<string>
+#define pii pair<ll,ll>
+#define ump unordered_map
+#define mp map
+#define pq_max priority_queue<ll>
+#define pq_min priority_queue<ll,vi,greater<ll>>
+#define ff first
+#define ss second
+#define mid(l,r) (l+(r-l)/2)
+#define loop(i,a,b) for(ll i=a;i<b;i++)
+#define rloop(i,a,b) for(ll i=a;i>=b;i--)
+#define logarr(arr,a,b) for(ll i=a;i<b;i++) cout<<arr[i]<<' '; cout<<endl;
+void file_i_o()
+{
+ios_base::sync_with_stdio(0);
+cin.tie(0);
+cout.tie(0);
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif
+}
+int main(int argc, char const *argv[])
+{
+clock_t begin = clock();
+file_i_o();
+#ifndef ONLINE_JUDGE
+clock_t end = clock();
+cout<<"Time: "<<(double)(end-begin)/CLOCKS_PER_SEC*1000<<"s\n";
+#endif
+int n;
+cin >> n;
+
+for (int row = -n; row <= n; ++row)
+{
+    int top = n - abs(row);
+    for (int i = 0; i < abs(row); ++i)
+    {
+        cout << "  ";
+    }
+    for (int i = 0; i < top; ++i)
+    {
+        cout << i << " ";
+    }
+    for (int i = top; i > 0; --i)
+    {
+        cout << i << " ";
+    }
+    cout << 0 << endl;
+}
+return 0;
+}
