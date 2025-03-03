d520f9ee9bcbbae687a088d38df38160cf8aaa48
Gaurav <gmpaliwal21@gmail.com>
2025-03-04 00:34:35 +0530

    Caps-Lock

---
+++ CodeForces/A/131A.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define ld  long double
+#define mod 1000000007
+#define inf 1e18
+#define pb push_back
+#define vi vector<ll>
+#define vs vector<string>
+#define pii pair<ll,ll>
+#define ump unordered_map
+#define mp map
+#define pq_max priority_queue<ll>
+#define pq_min priority_queue<ll,vi,greater<ll>>
+#define ff first
+#define ss second
+#define mid(l,r) (l+(r-l)/2)
+#define loop(i,a,b) for(ll i=a;i<b;i++)
+#define rloop(i,a,b) for(ll i=a;i>=b;i--)
+#define logarr(arr,a,b) for(ll i=a;i<b;i++) cout<<arr[i]<<' '; cout<<endl;
+void file_i_o()
+{
+ios_base::sync_with_stdio(0);
+cin.tie(0);
+cout.tie(0);
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif
+}
+int main(int argc, char const *argv[])
+{
+clock_t begin = clock();
+file_i_o();
+#ifndef ONLINE_JUDGE
+clock_t end = clock();
+cout<<"Time: "<<(double)(end-begin)/CLOCKS_PER_SEC*1000<<"s\n";
+#endif
+string word;
+cin >>word;
+
+bool allCaps = all_of(word.begin(), word.end(), ::isupper);
+bool firstLowerRestCaps = islower(word[0]) && all_of(word.begin() + 1, word.end(), ::isupper);
+if (allCaps || firstLowerRestCaps) {
+    for (char &c : word) {
+        c = isupper(c) ? tolower(c) : toupper(c);
+    }
+}
+
+cout << word << endl;
+return 0;
+}
