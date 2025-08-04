2e2264c9eeaa44012c566b0a3ece2be5e0a664bc
Jackal <gmpaliwal21@gmail.com>
2025-08-04 11:52:45 +0530

    POTD - 904

---
+++ POTD/July 2025/totalFruits.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define ld long double
+#define mod 1000000007
+#define inf 1e18
+#define pb push_back
+#define vi vector<ll>
+#define vs vector<string>
+#define pii pair<ll, ll>
+#define ump unordered_map
+#define mp map
+#define pq_max priority_queue<ll>
+#define pq_min priority_queue<ll, vi, greater<ll>>
+#define ff first
+#define ss second
+#define mid(l, r) (l + (r - l) / 2)
+#define loop(i, a, b) for (ll i = a; i < b; i++)
+#define rloop(i, a, b) for (ll i = a; i >= b; i--)
+#define logarr(arr, a, b)      \
+    for (ll i = a; i < b; i++) \
+        cout << arr[i] << ' '; \
+    cout << endl;
+void file_i_o()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+int main(int argc, char const *argv[])
+{
+    clock_t begin = clock();
+    file_i_o();
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "Time: " << (double)(end - begin) / CLOCKS_PER_SEC * 1000 << "s\n";
+#endif
+    vector<int> fruits = {0, 1, 2, 2};
+
+    unordered_map<int, int> mp;
+    int maxFruits = 0, l = 0;
+    for (auto r = 0; r < fruits.size(); r++)
+    {
+        mp[fruits[r]]++;
+
+        while (mp.size() > 2)
+        {
+            mp[fruits[l]]--;
+            if (mp[fruits[l]] == 0)
+            {
+                mp.erase(fruits[l]);
+            }
+            l++;
+        }
+
+        maxFruits = max(maxFruits, r - l + 1);
+    }
+
+    cout << maxFruits;
+
+    return 0;
+}
