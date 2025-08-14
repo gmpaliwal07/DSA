0f5423e5a48f62ebc6a1808f53b179a4bbc85a35
Gaurav <gmpaliwal21@gmail.com>
2025-08-15 01:14:12 +0530

    interview practice for amazon

---
+++ POTD/August 2025/largestGoodInteger.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define ld long double
+#define mod 1000000007
+#define inf 1e18
+#define pb push_back
+#define vi vector<ll>
+#define vs vector<string>
+#define pii pair<ll, ll>
+#define ump unordered_map
+#define mp map
+#define pq_max priority_queue<ll>
+#define pq_min priority_queue<ll, vi, greater<ll>>
+#define ff first
+#define ss second
+#define mid(l, r) (l + (r - l) / 2)
+#define loop(i, a, b) for (ll i = a; i < b; i++)
+#define rloop(i, a, b) for (ll i = a; i >= b; i--)
+#define logarr(arr, a, b)      \
+    for (ll i = a; i < b; i++) \
+        cout << arr[i] << ' '; \
+    cout << endl;
+void file_i_o()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+bool allvaluesSame(string s)
+{
+    return s.size() == 3 && s[0] == s[1] && s[1] == s[2];
+}
+string largestGoodInteger(string num)
+{
+    string maxi = "";
+    for(auto i = 0; i+2< num.length();i++) {
+        string s = num.substr(i, 3);
+        if(allvaluesSame(s)) {
+            if(maxi < s) {
+                maxi =s;
+
+            }
+        }
+    }
+    return maxi;
+}
+int main(int argc, char const *argv[])
+{
+    clock_t begin = clock();
+    file_i_o();
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "Time: " << (double)(end - begin) / CLOCKS_PER_SEC * 1000 << "s\n";
+#endif
+
+    string str = "6777133339";
+    cout << largestGoodInteger(str);
+
+    return 0;
+}
