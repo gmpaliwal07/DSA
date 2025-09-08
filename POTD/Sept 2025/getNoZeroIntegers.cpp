72244e796a81a01c2b63d37d6599894f51ac682a
Gaurav <gmpaliwal21@gmail.com>
2025-09-08 17:18:10 +0530

    POTD:1317

---
+++ POTD/Sept 2025/getNoZeroIntegers.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define ld  long double
+#define mod 1000000007
+#define inf 1e18
+#define pb push_back
+#define vi vector<ll>
+#define vs vector<string>
+#define pii pair<ll,ll>
+#define ump unordered_map
+#define mp map
+#define pq_max priority_queue<ll>
+#define pq_min priority_queue<ll,vi,greater<ll>>
+#define ff first
+#define ss second
+#define mid(l,r) (l+(r-l)/2)
+#define loop(i,a,b) for(ll i=a;i<b;i++)
+#define rloop(i,a,b) for(ll i=a;i>=b;i--)
+#define logarr(arr,a,b) for(ll i=a;i<b;i++) cout<<arr[i]<<' '; cout<<endl;
+void file_i_o()
+{
+ios_base::sync_with_stdio(0);
+cin.tie(0);
+cout.tie(0);
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif
+}
+class Solution {
+private:
+   bool hasZero(int x) {
+        while (x > 0) {
+            if (x % 10 == 0) return true;
+            x /= 10;
+        }
+        return false;
+    }
+public:
+    vector<int> getNoZeroIntegers(int n) {
+   vector<int> pair(2);
+        for (int a = 1; a < n; a++) {
+            int b = n - a;
+            if (!hasZero(a) && !hasZero(b)) {
+                pair[0] = a;
+                pair[1] = b;
+                break; 
+            }
+        }
+        return pair;
+    }
+};
+int main(int argc, char const *argv[])
+{
+clock_t begin = clock();
+file_i_o();
+#ifndef ONLINE_JUDGE
+clock_t end = clock();
+cout<<"Time: "<<(double)(end-begin)/CLOCKS_PER_SEC*1000<<"s\n";
+#endif
+int n = 11;
+Solution sol;
+vector<int> ans = sol.getNoZeroIntegers(n);
+cout << ans[0] << "," << ans[1];
+return 0;
+}
