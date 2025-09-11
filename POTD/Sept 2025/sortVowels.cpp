61e6d5010b57b042940d8653f94e591f7b6a6c2b
Gaurav <gmpaliwal21@gmail.com>
2025-09-12 01:10:52 +0530

    POTD:2785

---
+++ POTD/Sept 2025/sortVowels.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define ld  long double
+#define mod 1000000007
+#define inf 1e18
+#define pb push_back
+#define vi vector<ll>
+#define vs vector<string>
+#define pii pair<ll,ll>
+#define ump unordered_map
+#define mp map
+#define pq_max priority_queue<ll>
+#define pq_min priority_queue<ll,vi,greater<ll>>
+#define ff first
+#define ss second
+#define mid(l,r) (l+(r-l)/2)
+#define loop(i,a,b) for(ll i=a;i<b;i++)
+#define rloop(i,a,b) for(ll i=a;i>=b;i--)
+#define logarr(arr,a,b) for(ll i=a;i<b;i++) cout<<arr[i]<<' '; cout<<endl;
+void file_i_o()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+    string sortVowels(string &s) {
+        string vowels = "AEIOUaeiou";      
+        unordered_set<char> vowelSet(vowels.begin(), vowels.end());
+        
+        unordered_map<char,int> freq;
+        for(char c : s) {
+            if(vowelSet.count(c)) freq[c]++;
+        }
+        
+        string res = s;
+        int idx = 0;
+        for(int i=0; i<s.size(); i++) {
+            if(vowelSet.count(s[i])) {
+                while(idx < vowels.size() && freq[vowels[idx]] == 0) idx++;
+                res[i] = vowels[idx];
+                freq[vowels[idx]]--;
+            }
+        }
+        return res;
+    }
+
+int main(int argc, char const *argv[])
+{
+    clock_t begin = clock();
+    file_i_o();
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout<<"Time: "<<(double)(end-begin)/CLOCKS_PER_SEC*1000<<"s\n";
+#endif
+    
+    string s = "lEetcOde";
+    cout << sortVowels(s) << "\n";   // expected: lEOtcede
+    
+    return 0;
+}
