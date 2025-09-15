ebdbdd703d382597816fa1fb996a628825516d83
Gaurav <gmpaliwal21@gmail.com>
2025-09-15 10:43:17 +0530

    POTD:1935

---
+++ POTD/Sept 2025/canBeTypedWords.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define ld long double
+#define mod 1000000007
+#define inf 1e18
+#define pb push_back
+#define vi vector<ll>
+#define vs vector<string>
+#define pii pair<ll, ll>
+#define ump unordered_map
+#define mp map
+#define pq_max priority_queue<ll>
+#define pq_min priority_queue<ll, vi, greater<ll>>
+#define ff first
+#define ss second
+#define mid(l, r) (l + (r - l) / 2)
+#define loop(i, a, b) for (ll i = a; i < b; i++)
+#define rloop(i, a, b) for (ll i = a; i >= b; i--)
+#define logarr(arr, a, b)      \
+    for (ll i = a; i < b; i++) \
+        cout << arr[i] << ' '; \
+    cout << endl;
+void file_i_o()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+class Solution {
+    public:
+    int canBeTypedWords(string text, string brokenLetters)
+    {
+        int ans = 0;
+        string word = "";
+    
+        for (auto i = 0; i < text.length(); i++)
+        {
+            if (text[i] == ' ')
+            {
+                if (!word.empty() && word.find_first_of(brokenLetters) == string::npos)
+                    ans++;
+                word = "";
+            }
+            else
+            {
+                word += text[i];
+            }
+        }
+    
+        if (!word.empty() && word.find_first_of(brokenLetters) == string::npos)
+            ans++;
+    
+        return ans;
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    clock_t begin = clock();
+    file_i_o();
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "Time: " << (double)(end - begin) / CLOCKS_PER_SEC * 1000 << "s\n";
+#endif
+    string text = "leet code";
+    string broken = "e";
+    Solution sol;
+    cout << sol.canBeTypedWords(text, broken);
+
+    return 0;
+}
