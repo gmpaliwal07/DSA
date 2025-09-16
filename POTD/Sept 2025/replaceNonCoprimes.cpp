42139b213d9b20a81f3538ba6408c864069a7a77
Gaurav <gmpaliwal21@gmail.com>
2025-09-16 14:23:14 +0530

    POTD:2197

---
+++ POTD/Sept 2025/replaceNonCoprimes.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define ld long double
+#define mod 1000000007
+#define inf 1e18
+#define pb push_back
+#define vi vector<ll>
+#define vs vector<string>
+#define pii pair<ll, ll>
+#define ump unordered_map
+#define mp map
+#define pq_max priority_queue<ll>
+#define pq_min priority_queue<ll, vi, greater<ll>>
+#define ff first
+#define ss second
+#define mid(l, r) (l + (r - l) / 2)
+#define loop(i, a, b) for (ll i = a; i < b; i++)
+#define rloop(i, a, b) for (ll i = a; i >= b; i--)
+#define logarr(arr, a, b)      \
+    for (ll i = a; i < b; i++) \
+        cout << arr[i] << ' '; \
+    cout << endl;
+void file_i_o()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+class Solution
+{
+
+public:
+    vector<int> replaceNonCoprimes(vector<int> &nums)
+    {
+        vector<int> st;
+        for (int it : nums)
+        {
+            st.push_back(it);
+            while (st.size() > 1)
+            {
+                int a = st[st.size() - 2];
+                int b = st[st.size() - 1];
+                int g = gcd(a, b);
+                if (g > 1)
+                { 
+                    st.pop_back();
+                    st.pop_back();
+                    st.push_back(lcm(a, b));
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        return st;
+    }
+};
+int main(int argc, char const *argv[])
+{
+    clock_t begin = clock();
+    file_i_o();
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "Time: " << (double)(end - begin) / CLOCKS_PER_SEC * 1000 << "s\n";
+#endif
+    vector<int> nums = {6, 4, 3, 2, 7, 6, 2};
+    Solution sol;
+    vector<int> ans = sol.replaceNonCoprimes(nums);
+    for (auto it : ans)
+    {
+        cout << it << "\n";
+    }
+    return 0;
+}
