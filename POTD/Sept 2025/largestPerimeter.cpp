e1673df65709cbef9bc2f2eb196078a64141b01a
Gaurav <gmpaliwal21@gmail.com>
2025-09-28 22:18:07 +0530

    POTD:976

---
+++ POTD/Sept 2025/largestPerimeter.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define ld long double
+#define mod 1000000007
+#define inf 1e18
+#define pb push_back
+#define vi vector<ll>
+#define vs vector<string>
+#define pii pair<ll, ll>
+#define ump unordered_map
+#define mp map
+#define pq_max priority_queue<ll>
+#define pq_min priority_queue<ll, vi, greater<ll>>
+#define ff first
+#define ss second
+#define mid(l, r) (l + (r - l) / 2)
+#define loop(i, a, b) for (ll i = a; i < b; i++)
+#define rloop(i, a, b) for (ll i = a; i >= b; i--)
+#define logarr(arr, a, b)      \
+    for (ll i = a; i < b; i++) \
+        cout << arr[i] << ' '; \
+    cout << endl;
+void file_i_o()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+class Solution
+{
+public:
+    int largestPerimeter(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end(), greater<int>());
+
+   
+        for (int i = 0; i < nums.size() - 2; i++)
+        {
+            if (nums[i] < nums[i + 1] + nums[i + 2])
+            {
+                return nums[i] + nums[i + 1] + nums[i + 2];
+            }
+        }
+
+        return 0; 
+    }
+};
+int main(int argc, char const *argv[])
+{
+    clock_t begin = clock();
+    file_i_o();
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "Time: " << (double)(end - begin) / CLOCKS_PER_SEC * 1000 << "s\n";
+#endif
+
+    vector<int> nums = {2, 1, 2};
+Solution sol;
+cout << sol.largestPerimeter(nums);
+
+    return 0;
+}
