dd151893c663e9344de0e385f6e07964816b9f9e
Gaurav <gmpaliwal21@gmail.com>
2025-10-01 11:51:11 +0530

    POTD:1518

---
+++ POTD/October 2025/numWaterBottles.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define ld long double
+#define mod 1000000007
+#define inf 1e18
+#define pb push_back
+#define vi vector<ll>
+#define vs vector<string>
+#define pii pair<ll, ll>
+#define ump unordered_map
+#define mp map
+#define pq_max priority_queue<ll>
+#define pq_min priority_queue<ll, vi, greater<ll>>
+#define ff first
+#define ss second
+#define mid(l, r) (l + (r - l) / 2)
+#define loop(i, a, b) for (ll i = a; i < b; i++)
+#define rloop(i, a, b) for (ll i = a; i >= b; i--)
+#define logarr(arr, a, b)      \
+    for (ll i = a; i < b; i++) \
+        cout << arr[i] << ' '; \
+    cout << endl;
+void file_i_o()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+class Solution
+{
+public:
+    int numWaterBottles(int numBottles, int numExchange)
+    {
+        int sum = numBottles;
+        int curr_empty = numBottles;
+        while (curr_empty >= numExchange)
+        {
+            int newBottles = curr_empty / numExchange;
+            sum += newBottles;
+            curr_empty = newBottles + (curr_empty % numExchange);
+        }
+        return sum;
+    }
+};
+int main(int argc, char const *argv[])
+{
+    clock_t begin = clock();
+    file_i_o();
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "Time: " << (double)(end - begin) / CLOCKS_PER_SEC * 1000 << "s\n";
+#endif
+
+    int numBottles = 15, numExchage = 4;
+
+    Solution sol;
+    cout << sol.numWaterBottles(numBottles, numExchage);
+
+    return 0;
+}
