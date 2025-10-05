c69da309359cf72276e8942ab1564b24158f76de
Gaurav <gmpaliwal21@gmail.com>
2025-10-06 00:17:27 +0530

    1391:B Fix You

--- CodeForces/B/1391B.cpp
+++ CodeForces/B/1391B.cpp
@@ -1,58 +1,80 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 #define ll long long int
-#define ld  long double
+#define ld long double
 #define mod 1000000007
 #define inf 1e18
 #define pb push_back
 #define vi vector<ll>
 #define vs vector<string>
-#define pii pair<ll,ll>
+#define pii pair<ll, ll>
 #define ump unordered_map
 #define mp map
 #define pq_max priority_queue<ll>
-#define pq_min priority_queue<ll,vi,greater<ll>>
+#define pq_min priority_queue<ll, vi, greater<ll>>
 #define ff first
 #define ss second
-#define mid(l,r) (l+(r-l)/2)
-#define loop(i,a,b) for(ll i=a;i<b;i++)
-#define rloop(i,a,b) for(ll i=a;i>=b;i--)
-#define logarr(arr,a,b) for(ll i=a;i<b;i++) cout<<arr[i]<<' '; cout<<endl;
+#define mid(l, r) (l + (r - l) / 2)
+#define loop(i, a, b) for (ll i = a; i < b; i++)
+#define rloop(i, a, b) for (ll i = a; i >= b; i--)
+#define logarr(arr, a, b)      \
+    for (ll i = a; i < b; i++) \
+        cout << arr[i] << ' '; \
+    cout << endl;
+
 void file_i_o()
 {
-ios_base::sync_with_stdio(0);
-cin.tie(0);
-cout.tie(0);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 #ifndef ONLINE_JUDGE
-freopen("input.txt", "r", stdin);
-freopen("output.txt", "w", stdout);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
 #endif
 }
+
 int main(int argc, char const *argv[])
 {
-clock_t begin = clock();
-file_i_o();
+    clock_t begin = clock();
+    file_i_o();
 #ifndef ONLINE_JUDGE
-clock_t end = clock();
-cout<<"Time: "<<(double)(end-begin)/CLOCKS_PER_SEC*1000<<"s\n";
+    clock_t end = clock();
+    cout << "Time: " << (double)(end - begin) / CLOCKS_PER_SEC * 1000 << "s\n";
 #endif
-int t;
-cin >> t;
 
+    int t;
+    cin >> t;
 
-while (t--) {
-    int r, c;
-    cin >> r >> c;
-    
-    vector<vector<char>> chars(r, vector<char>(c)); 
-    for (int i = 0; i < r; i++) {
-        for (int j = 0; j < c; j++) { 
-            cin >> chars[i][j]; 
+    while (t--)
+    {
+        int r, c;
+        cin >> r >> c;
+
+        vector<vector<char>> dirs(r, vector<char>(c));
+        for (int i = 0; i < r; i++)
+        {
+            for (int j = 0; j < c; j++)
+            {
+                cin >> dirs[i][j];
+            }
         }
+
+        int cnt = 0;
+
+        for (int j = 0; j < c - 1; j++)
+        {
+            if (dirs[r - 1][j] != 'R')
+                cnt++;
+        }
+
+        for (int i = 0; i < r - 1; i++)
+        {
+            if (dirs[i][c - 1] != 'D')
+                cnt++;
+        }
+
+        cout << cnt << "\n";
     }
-}
 
-
-
-return 0;
+    return 0;
 }
