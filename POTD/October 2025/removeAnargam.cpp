b29ea16b7b11576b8e06b51137815ef3d9abecbf
Gaurav <gmpaliwal21@gmail.com>
2025-10-13 17:31:31 +0530

    POTD : 2273

---
+++ POTD/October 2025/removeAnargam.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define ld long double
+#define mod 1000000007
+#define inf 1e18
+#define pb push_back
+#define vi vector<ll>
+#define vs vector<string>
+#define pii pair<ll, ll>
+#define ump unordered_map
+#define mp map
+#define pq_max priority_queue<ll>
+#define pq_min priority_queue<ll, vi, greater<ll>>
+#define ff first
+#define ss second
+#define mid(l, r) (l + (r - l) / 2)
+#define loop(i, a, b) for (ll i = a; i < b; i++)
+#define rloop(i, a, b) for (ll i = a; i >= b; i--)
+#define logarr(arr, a, b)      \
+    for (ll i = a; i < b; i++) \
+        cout << arr[i] << ' '; \
+    cout << endl;
+void file_i_o()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+class Solution
+{
+public:
+    bool ValidAnagram(string s, string t)
+    {
+        if (s.size() != t.size())
+            return false;
+
+        unordered_map<char, int> mpp;
+        for (char c : s)
+            mpp[c]++;
+        for (char c : t)
+            mpp[c]--;
+
+        for (auto &p : mpp)
+        {
+            if (p.second != 0)
+                return false;
+        }
+        return true;
+    }
+
+    vector<string> removeAnagrams(vector<string> &words)
+    {
+        int i = 1;
+        while (i < words.size())
+        {
+            if (ValidAnagram(words[i - 1], words[i]))
+            {
+                words.erase(words.begin() + i);
+            }
+            else
+            {
+                i++;
+            }
+        }
+        return words;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    clock_t begin = clock();
+    file_i_o();
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "Time: " << (double)(end - begin) / CLOCKS_PER_SEC * 1000 << "s\n";
+#endif
+    vector<string> words = {"abba", "baba", "bbaa", "cd", "cd"};
+    Solution sol;
+    vector<string> ans = sol.removeAnagrams(words);
+    for(auto it : ans) {
+        cout << it << " ";
+    }
+     return 0;
+}
